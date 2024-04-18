ffcd161c68905f4c03b68e51c5094352b48a67c5
Ph0sph0rrr <ilyushka.velikanov@mail.ru>
2024-04-18 07:20:43 +0300

    pm
    pm

---
+++ pm/Form2.cs
@@ -0,0 +1,101 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pm
+{
+    public partial class Form2 : Form
+    {
+        Database db = new Database();
+
+        public Form2()
+        {
+            
+
+
+            InitializeComponent();
+            
+
+
+
+            
+
+
+
+        }
+
+        private void btnEnt_Click(object sender, EventArgs e)
+        {
+            List<int> reqs = new List<int>();
+
+            db.openConnection();
+
+            NpgsqlCommand comm = new NpgsqlCommand("SELECT id FROM users WHERE stat = 'Подано'", db.getConnection());
+            NpgsqlDataReader reader = comm.ExecuteReader();
+
+            while (reader.Read())
+            {
+                reqs.Add(reader.GetInt32(0));
+            }
+
+            db.closeConnection();
+
+
+
+
+
+
+
+
+
+
+            
+        }
+
+        private int countReqs()
+        {
+            List<int> reqs = new List<int>();
+
+            db.openConnection();
+
+            NpgsqlCommand comm = new NpgsqlCommand("SELECT id FROM users WHERE stat = 'Подано'", db.getConnection());
+            NpgsqlDataReader reader = comm.ExecuteReader();
+
+            while (reader.Read())
+            {
+                reqs.Add(reader.GetInt32(0));
+            }
+
+            db.closeConnection();
+
+            return reqs.Count();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            
+
+
+        }
+
+        private void regCl_Click(object sender, EventArgs e)
+        {
+            db.openConnection();
+
+            NpgsqlCommand updateDatFin = new NpgsqlCommand($"UPDATE users SET fio = '{tBxIoRg.Text}',  WHERE id = '{idd.Text}'", db.getConnection());
+            updateDatFin.ExecuteNonQuery();
+
+            db.closeConnection();
+
+            MessageBox.Show("Дата окончания изменена");
+        }
+    }
+}
---
+++ pm/Form3.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace pm
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lbOftProb = new System.Windows.Forms.Label();
+            this.lbMedTime = new System.Windows.Forms.Label();
+            this.lbDoneReq = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(729, 357);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBack.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBack.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBack.Location = new System.Drawing.Point(225, 158);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(111, 39);
+            this.btnBack.TabIndex = 37;
+            this.btnBack.Text = "Назад";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // lbOftProb
+            // 
+            this.lbOftProb.AutoSize = true;
+            this.lbOftProb.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbOftProb.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lbOftProb.Location = new System.Drawing.Point(618, 202);
+            this.lbOftProb.Name = "lbOftProb";
+            this.lbOftProb.Size = new System.Drawing.Size(264, 25);
+            this.lbOftProb.TabIndex = 36;
+            this.lbOftProb.Text = "Частая неисправность:";
+            // 
+            // lbMedTime
+            // 
+            this.lbMedTime.AutoSize = true;
+            this.lbMedTime.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbMedTime.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lbMedTime.Location = new System.Drawing.Point(655, 168);
+            this.lbMedTime.Name = "lbMedTime";
+            this.lbMedTime.Size = new System.Drawing.Size(213, 20);
+            this.lbMedTime.TabIndex = 35;
+            this.lbMedTime.Text = "Среднее время выполнения:";
+            // 
+            // lbDoneReq
+            // 
+            this.lbDoneReq.AutoSize = true;
+            this.lbDoneReq.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbDoneReq.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.lbDoneReq.Location = new System.Drawing.Point(352, 168);
+            this.lbDoneReq.Name = "lbDoneReq";
+            this.lbDoneReq.Size = new System.Drawing.Size(215, 20);
+            this.lbDoneReq.TabIndex = 34;
+            this.lbDoneReq.Text = "Кол-во выполненых заказов:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label1.Location = new System.Drawing.Point(247, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 25);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Приоритетные заявки:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(240, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(804, 420);
+            this.pictureBox1.TabIndex = 32;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(794, 27);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(313, 138);
+            this.flowLayoutPanel1.TabIndex = 38;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 636);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.lbOftProb);
+            this.Controls.Add(this.lbMedTime);
+            this.Controls.Add(this.lbDoneReq);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button btnBack;
+        private Label lbOftProb;
+        private Label lbMedTime;
+        private Label lbDoneReq;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+    }
+}
